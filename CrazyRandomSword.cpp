e1c767cc39da435197efb16729ba52bb2bfe5b03
Hannah Howard <hlh16b@my.fsu.edu>
2019-01-31 23:14:21 -0500

    Created Crazy Random Sword
    Created the Crazy Random Sword file; has a randomized number generated
    at the creation of the sword as the hitpoints, as well as for damage.

---
+++ CrazyRandomSword.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   CrazyRandomSword.h
+ * Author: Hannah Howard <hlh16b@my.fsu.edu>
+ *  
+ */
+
+#include "CrazyRandomSword.h"
+
+double CrazyRandomSword::hit(double armor)
+{
+	srand(time(NULL));
+	int temp = rand() % (armor/3) + 2;
+	
+	double damage = hitpoints - (armor - temp);
+	
+	if(damage < 0)
+		return 0;
+	return damage;
+}
---
+++ CrazyRandomSword.h
@@ -0,0 +1,28 @@
+/* 
+ * File:   CrazyRandomSword.h
+ * Author: Hannah Howard <hlh16b@my.fsu.edu>
+ *  
+ */
+
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include "Weapon.h"
+
+#ifndef CRAZYRANDOMSWORD_H
+#define CRAZYRANDOMSWORD_H
+
+class CrazyRandomSword : public Weapon
+{
+	public:
+		/* Note: I gave it hitpoints the value of 7 initially just to give it something, 
+		and change it in the body of the constructor, as hitpoints is a protected variable 
+		and accessible from here */
+		CrazyRandomSword() : Weapon("Crazy random sword", 7)	
+		{
+			srand(time(NULL));
+			hitpoints = rand() % 94 + 7;
+			
+			// Referred to the C++ reference "rand" page to see how to do this
+		}
+}
