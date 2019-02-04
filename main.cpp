1458a3724e4faf7520e393ea56394d4b983fd10c
Hannah Howard <hlh16b@my.fsu.edu>
2019-02-03 20:43:08 -0500

    Added test cases for new weapons

--- main.cpp
+++ main.cpp
@@ -38,6 +38,72 @@ int main(int argc, char** argv) {
     weapon = WeaponFactory::getInstance()->getWeapon("spear");
     simulateWeapon(weapon, armor);
     delete(weapon);
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("hammer");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("crazy sword");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("tonbogiri");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	armor = 37;
+
+	weapon = WeaponFactory::getInstance()->getWeapon("hammer");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("crazy sword");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("tonbogiri");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	armor = 45;
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("crazy sword");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("tonbogiri");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	armor = 77;
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("crazy sword");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("tonbogiri");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	armor = 128;
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("crazy sword");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("tonbogiri");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	armor = 36;
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("crazy sword");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
+	
+	weapon = WeaponFactory::getInstance()->getWeapon("tonbogiri");
+    simulateWeapon(weapon, armor);
+    delete(weapon);
 
     return 0;
 }
